529603654f7a68a6d2ac0a96ef947fee1c405186
Tom Lamb <tmlamb@gmail.com>
2022-09-27 12:16:19 -0500

    debugging gcloud issue

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -50,6 +50,36 @@ users:
       provideClusterInfo: true
 `;
   });
+// export const kubeconfig = pulumi.
+//     all([ cluster.name, cluster.endpoint, cluster.masterAuth ]).
+//     apply(([ name, endpoint, masterAuth ]) => {
+//         const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+//         return `apiVersion: v1
+// clusters:
+// - cluster:
+//     certificate-authority-data: ${masterAuth.clusterCaCertificate}
+//     server: https://${endpoint}
+//   name: ${context}
+// contexts:
+// - context:
+//     cluster: ${context}
+//     user: ${context}
+//   name: ${context}
+// current-context: ${context}
+// kind: Config
+// preferences: {}
+// users:
+// - name: ${context}
+//   user:
+//     auth-provider:
+//       config:
+//         cmd-args: config config-helper --format=json
+//         cmd-path: gcloud
+//         expiry-key: '{.credential.token_expiry}'
+//         token-key: '{.credential.access_token}'
+//       name: gcp
+// `;
+//     });
 
 // Create a Kubernetes provider instance that uses our cluster from above.
 const clusterProvider = new k8s.Provider(name, {
