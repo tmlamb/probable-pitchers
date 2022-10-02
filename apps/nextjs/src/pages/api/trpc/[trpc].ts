55cdec45d8c6b966332194914cf4dda20f82c7dd
Tom Lamb <tmlamb@gmail.com>
2022-10-02 10:31:29 -0500

    fixed backend import

--- apps/nextjs/src/pages/api/trpc/[trpc].ts
+++ apps/nextjs/src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,5 @@
 // src/pages/api/trpc/[trpc].ts
-import { appRouter, createContext } from "@probable/api";
+import { appRouter, createContext } from "@probable/api/backend";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 // export API handler
