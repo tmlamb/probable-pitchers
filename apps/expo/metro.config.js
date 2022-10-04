a1c77f1fb8facfc226242d716c876e2a56f9a6a8
Tom Lamb <tmlamb@gmail.com>
2022-10-04 08:58:30 -0500

    configuring eas builds

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  resolver: {
-    /* resolver options */
-    sourceExts: ["jsx", "js", "ts", "tsx", "cjs"],
-  },
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts = ["jsx", "js", "ts", "tsx", "cjs"];
+
+module.exports = config;
