3388b705c14849a3a0c9a68c0133eb896fb45a4f
Tom Lamb <tmlamb@gmail.com>
2022-10-05 19:55:02 -0500

    privacy policy

--- apps/nextjs/src/pages/privacy.tsx
+++ apps/nextjs/src/pages/privacy.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-const Home: NextPage = () => {
+const Privacy: NextPage = () => {
   return (
     <>
       <Head>
@@ -13,12 +13,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container flex flex-col items-center justify-center min-h-screen p-4 mx-auto">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Probable Pitcher
+        <h1 className="text-5xl md:text-[2rem] leading-normal font-extrabold text-gray-700">
+          Privacy Policy
         </h1>
+        <p>
+          Probable Pitcher does not collect any personal information. If despite
+          this, any of your personal information were to be obtained by the app,
+          we would not sell or give it to anyone.
+        </p>
       </main>
     </>
   );
 };
 
-export default Home;
+export default Privacy;
