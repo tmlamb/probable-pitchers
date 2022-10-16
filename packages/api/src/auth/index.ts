c046f30cb85583a5f22d50347525a44b2042d33e
Tom Lamb <tmlamb@gmail.com>
2022-10-16 09:17:28 -0500

    apple signin wip

--- packages/api/src/auth/index.ts
+++ packages/api/src/auth/index.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { DefaultUser } from "next-auth";
 import NextAuth, { type NextAuthOptions } from "next-auth";
+import AppleProvider from "next-auth/providers/apple";
 import GoogleProvider from "next-auth/providers/google";
 
 import { prisma } from "@probable/db";
@@ -16,8 +17,33 @@ declare module "next-auth" {
 
 const adapter = PrismaAdapter(prisma);
 export const authOptions: NextAuthOptions = {
+  debug: true,
   adapter,
   providers: [
+    AppleProvider({
+      clientId: process.env.APPLE_CLIENT_ID as string,
+      clientSecret: process.env.APPLE_CLIENT_SECRET as string,
+    }),
+    {
+      ...AppleProvider({
+        name: "Apple Expo Proxy",
+        checks: ["state", "pkce"],
+        clientId: process.env.APPLE_CLIENT_ID as string,
+        clientSecret: process.env.APPLE_CLIENT_SECRET as string,
+        token: {
+          async request(context) {
+            // console.log(JSON.stringify(context));
+            const tokens = await context.client.callback(
+              process.env.NEXTAUTH_EXPO_URL,
+              context.params,
+              context.checks
+            );
+            return { tokens };
+          },
+        },
+        id: nativeProviders.apple,
+      }),
+    },
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET as string,
--- packages/api/src/auth/providers.ts
+++ packages/api/src/auth/providers.ts
@@ -1,5 +1,6 @@
 export const nativeProviders = {
   google: "google-expo",
+  apple: "apple-expo",
 } as const;
 
 export const isValidProvider = (
@@ -8,18 +9,18 @@ export const isValidProvider = (
   return k in nativeProviders;
 };
 
-export const nativeDiscoveryUrls = {
-  google: {
-    authorizationEndpoint: "https://accounts.google.com/o/oauth2/auth",
-    tokenEndpoint: "https://accounts.google.com/o/oauth2/token",
-    revocationEndpoint: "https://accounts.google.com/o/oauth2/revoke",
-  },
-};
+// export const nativeDiscoveryUrls = {
+//   google: {
+//     authorizationEndpoint: "https://accounts.google.com/o/oauth2/auth",
+//     tokenEndpoint: "https://accounts.google.com/o/oauth2/token",
+//     revocationEndpoint: "https://accounts.google.com/o/oauth2/revoke",
+//   },
+// };
 
-export const getDiscoveryUrls = (provider: keyof typeof nativeProviders) => {
-  if (!isValidProvider(provider))
-    throw new Error(
-      `Could not find discovery for ${provider} or the provider provided is not correct. Have you typed it in correctly? If you need to add a provider discovery check 'packages/server/src/auth/providers.ts'`
-    );
-  return nativeDiscoveryUrls[provider];
-};
+// export const getDiscoveryUrls = (provider: keyof typeof nativeProviders) => {
+//   if (!isValidProvider(provider))
+//     throw new Error(
+//       `Could not find discovery for ${provider} or the provider provided is not correct. Have you typed it in correctly? If you need to add a provider discovery check 'packages/server/src/auth/providers.ts'`
+//     );
+//   return nativeDiscoveryUrls[provider];
+// };
