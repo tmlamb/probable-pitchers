d4b41495ed90af88565c0e46e1c9f4f917c6bd09
Tom Lamb <tmlamb@gmail.com>
2022-11-03 22:57:00 -0500

    fixed tailwind theme regression

--- apps/expo/src/App.tsx
+++ apps/expo/src/App.tsx
@@ -5,9 +5,11 @@ import React from "react";
 import { Platform } from "react-native";
 import { enableLayoutAnimations } from "react-native-reanimated";
 import * as Sentry from "sentry-expo";
+import { useDeviceContext } from "twrnc";
 import AuthProvider from "./components/AuthProvider";
 import { Navigation } from "./components/Navigation";
 import TRPCProvider from "./components/TRPCProvider";
+import tw from "./tailwind";
 
 const { sentryPublicDsn, appEnv } = Constants.expoConfig?.extra || {};
 if (sentryPublicDsn) {
@@ -19,6 +21,9 @@ if (sentryPublicDsn) {
 }
 
 export default function App() {
+  // https://github.com/jaredh159/tailwind-react-native-classnames#enabling-device-context-prefixes
+  useDeviceContext(tw);
+
   if (Platform.OS === "android") {
     enableLayoutAnimations(false);
   }
