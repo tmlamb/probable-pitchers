f5ffb45d06ea67e2be2442a50a926b1420270e60
Tom Lamb <tmlamb@gmail.com>
2022-11-06 09:23:05 -0600

    redeploy gmp

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -386,30 +386,30 @@ const ingress = new k8s.networking.v1.Ingress(
   }
 );
 
-// const podMon = new k8s.apiextensions.CustomResource(
-//   `probable-pod-mon-${env}`,
-//   {
-//     apiVersion: "monitoring.googleapis.com/v1",
-//     kind: "PodMonitoring",
-//     metadata: {
-//       namespace: namespaceName,
-//     },
-//     spec: {
-//       selector: {
-//         matchLabels: {
-//           app: service.metadata.apply((m) => m.name),
-//         },
-//       },
-//       endpoints: [
-//         {
-//           port: 80,
-//           path: "/api/metrics",
-//           interval: "30s",
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     provider: clusterProvider,
-//   }
-// );
+const podMon = new k8s.apiextensions.CustomResource(
+  `probable-pod-mon-${env}`,
+  {
+    apiVersion: "monitoring.googleapis.com/v1",
+    kind: "PodMonitoring",
+    metadata: {
+      namespace: namespaceName,
+    },
+    spec: {
+      selector: {
+        matchLabels: {
+          app: service.metadata.apply((m) => m.name),
+        },
+      },
+      endpoints: [
+        {
+          port: 80,
+          path: "/api/metrics",
+          interval: "30s",
+        },
+      ],
+    },
+  },
+  {
+    provider: clusterProvider,
+  }
+);
