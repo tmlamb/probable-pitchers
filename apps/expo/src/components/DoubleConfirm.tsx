a6ac9b99ca847dbb086283533047262d5e92d000
Tom Lamb <tmlamb@gmail.com>
2022-11-16 21:46:16 -0600

    added missing dep

--- apps/expo/src/components/DoubleConfirm.tsx
+++ apps/expo/src/components/DoubleConfirm.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import { View } from 'react-native'
-import Animated, { RollInRight, RollOutRight } from 'react-native-reanimated'
-import { ClassInput } from 'twrnc/dist/esm/types'
-import tw from '../tailwind'
-import ButtonContainer from './ButtonContainer'
+import React from "react";
+import { View } from "react-native";
+import Animated, { RollInRight, RollOutRight } from "react-native-reanimated";
+import { ClassInput } from "twrnc/dist/esm/types";
+import tw from "../tailwind";
+import ButtonContainer from "./ButtonContainer";
 
 type Props = {
-  style?: ClassInput
-  first: JSX.Element
-  second: JSX.Element
-  accessibilityLabel: string
-}
+  style?: ClassInput;
+  first: JSX.Element;
+  second: JSX.Element;
+  accessibilityLabel: string;
+};
 
-export default function DoubleConfirm({ style, first, second, accessibilityLabel }: Props) {
-  const [toggle, setToggle] = React.useState(false)
+export default function DoubleConfirm({
+  style,
+  first,
+  second,
+  accessibilityLabel,
+}: Props) {
+  const [toggle, setToggle] = React.useState(false);
 
   return (
-    <View style={tw.style('relative', style)}>
+    <View style={tw.style("relative", style)}>
       <ButtonContainer
         onPress={() => {
-          setToggle(!toggle)
+          setToggle(!toggle);
         }}
-        accessibilityLabel={`${toggle ? 'Conceal' : 'Reveal'} button to ${accessibilityLabel}`}
+        accessibilityLabel={`${
+          toggle ? "Conceal" : "Reveal"
+        } button to ${accessibilityLabel}`}
       >
         {first}
       </ButtonContainer>
@@ -29,7 +36,9 @@ export default function DoubleConfirm({ style, first, second, accessibilityLabel
         <Animated.View
           entering={RollInRight.springify().stiffness(50).damping(6).mass(0.3)}
           exiting={RollOutRight.springify().stiffness(50).damping(6).mass(0.3)}
-          style={[tw.style('absolute items-center justify-center h-full right-0')]}
+          style={[
+            tw.style("absolute items-center justify-center h-full right-0"),
+          ]}
           accessible
           accessibilityLabel={`${accessibilityLabel}`}
           accessibilityRole="button"
@@ -38,9 +47,9 @@ export default function DoubleConfirm({ style, first, second, accessibilityLabel
         </Animated.View>
       )}
     </View>
-  )
+  );
 }
 
 DoubleConfirm.defaultProps = {
-  style: undefined
-}
+  style: undefined,
+};
--- apps/expo/src/components/Navigation/index.tsx
+++ apps/expo/src/components/Navigation/index.tsx
@@ -1,3 +1,3 @@
-export * from './Navigation'
-export { default as Navigation } from './Navigation'
-export * from './types'
+export * from "./Navigation";
+export { default as Navigation } from "./Navigation";
+export * from "./types";
