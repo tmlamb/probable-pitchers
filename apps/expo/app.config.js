e60f45efbc857a3bace4e4e0d325f22b81376e2f
Tom Lamb <tmlamb@gmail.com>
2022-11-27 21:56:29 -0600

    preview bi fix

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -4,6 +4,18 @@ export default ({ config }) => {
   const appConfig = {
     ...config,
     name: `${config.name}${APP_ENV !== "production" ? ` (${APP_ENV})` : ""}`,
+    ios: {
+      ...config.ios,
+      bundleIdentifier: `${config.ios.bundleIdentifier}${
+        APP_ENV !== "production" ? `.${APP_ENV}` : ""
+      }`,
+    },
+    android: {
+      ...config.android,
+      package: `${config.android.package}${
+        APP_ENV !== "production" ? `.${APP_ENV}` : ""
+      }`,
+    },
     extra: {
       ...config.extra,
       apiBaseUrl: API_BASE_URL,
