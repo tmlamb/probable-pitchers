b7d2fab8a601c9399b0f56697ad68e8110a1d1c5
Tom Lamb <tmlamb@gmail.com>
2023-03-05 01:21:20 -0600

    expo update only

--- apps/expo/src/screens/Support.tsx
+++ apps/expo/src/screens/Support.tsx
@@ -14,7 +14,7 @@ export const Support = () => {
         contentContainerStyle={tw`justify-between px-3 pt-9 pb-9 flex-1`}
       >
         <View style={tw``}>
-          <PrimaryText style={tw`text-lg mb-4`}>FAQs</PrimaryText>
+          <PrimaryText style={tw`text-lg mb-4`}>Frequently Asked Questions</PrimaryText>
           {faqs.map((faq) => (
             <View key={faq.question} style={tw`mb-4`}>
               <PrimaryText style={tw`font-bold text-base`}>
@@ -28,7 +28,7 @@ export const Support = () => {
           onPress={() =>
             Linking.openURL(
               "https://github.com/tmlamb/probable-pitchers/issues"
-            )
+             )
           }
           accessibilityRole="link"
           accessibilityLabel="Open Application Feedback Page In Browser"
