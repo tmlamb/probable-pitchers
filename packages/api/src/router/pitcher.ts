77c6db6c449529ea4792b6d58cacdb33f7789db1
Tom Lamb <tmlamb@gmail.com>
2023-03-25 11:56:06 -0500

    added pitcher number and team abbreviation to api

--- packages/api/src/router/pitcher.ts
+++ packages/api/src/router/pitcher.ts
@@ -3,37 +3,6 @@ import { z } from "zod";
 import { isAuthed, t } from "../trpc";
 
 export const pitcherRouter = t.router({
-  byNameSearch: t.procedure
-    .use(isAuthed)
-    .input(z.array(z.string()))
-    .query(async ({ ctx, input }) => {
-      const inputJoined = input.join(" ");
-      const searchResult: Pitcher[] = await ctx.prisma.pitcher.findMany({
-        where: {
-          OR: [
-            {
-              name: { search: input.join(" | ") },
-            },
-            {
-              name: { contains: inputJoined },
-            },
-          ],
-        },
-        orderBy: {
-          name: "asc",
-        },
-      });
-
-      const result = searchResult.sort((a, b) => {
-        const startsWithA = a.name.startsWith(inputJoined);
-        const startsWithB = b.name.startsWith(inputJoined);
-        if (startsWithA && !startsWithB) return -1;
-        if (!startsWithA && startsWithB) return 1;
-        return 0;
-      });
-
-      return result;
-    }),
   byFuzzyName: t.procedure
     .use(isAuthed)
     .input(z.string())
@@ -44,9 +13,17 @@ export const pitcherRouter = t.router({
         `${acc}${!!index && index < words.length ? ' ' : ''}*${word}*`, ''
       );
 
-      return await ctx.prisma.$queryRaw<Pitcher[]>`
-        SELECT * FROM Pitcher
-        WHERE MATCH (name) 
+      return await ctx.prisma.$queryRaw<(Pitcher & { team?: { abbreviation?: string } })[]>`
+        SELECT
+            p.id,
+            p.name,
+            p.teamId,
+            p.primaryNumber,
+            t.abbreviation
+        FROM
+            Pitcher p
+        INNER JOIN Team t ON t.id = p.teamId
+        WHERE MATCH (p.name)
         AGAINST (${query} IN BOOLEAN MODE)
       `
     }),
