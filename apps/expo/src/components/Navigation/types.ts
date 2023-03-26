bca185df2f8674d149160a8bca2ad6082839107c
Tom Lamb <tmlamb@gmail.com>
2023-03-26 14:59:09 -0500

    removed Home page from types

--- apps/expo/src/components/Navigation/types.ts
+++ apps/expo/src/components/Navigation/types.ts
@@ -1,7 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
-  Home: undefined;
   Subscriptions: undefined;
   Welcome: undefined;
   Settings: undefined;
