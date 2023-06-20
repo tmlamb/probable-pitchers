4f5fef3517e2589d5a1cd911370f4e15e2bb5b90
Tom Lamb <tmlamb@gmail.com>
2023-06-19 20:02:46 -0500

    lint fix

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -1,3 +1,4 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const path = require("path");
