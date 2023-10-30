dd12b29b53e8b4fb7c57a7e4e1bb21caa0d89711
Tom Lamb <tmlamb@gmail.com>
2023-10-29 23:06:07 -0500

    Node18 (#30)

--- apps/k6/scripts/index.js
+++ apps/k6/scripts/index.js
@@ -1,9 +1,9 @@
 import http from "k6/http";
-import { check } from 'k6';
+import { check } from "k6";
 
 export const options = {
-  vus: 5,
-  duration: "60s",
+  vus: 4,
+  duration: "45s",
 };
 
 const names = [
@@ -30,27 +30,26 @@ const params = {
     "Content-Type": "application/json",
   },
   cookies: {
-    "next-auth.session-token":
-      "4c2358f5-51e0-4be5-ae8e-312a6b9888e8",
+    "next-auth.session-token": "4c2358f5-51e0-4be5-ae8e-312a6b9888e8",
   },
 };
 
-export default function() {
-
+export default function () {
   const searchTerms = [names[Math.floor(Math.random() * names.length)]];
 
   if (Math.random() > 0.5) {
     searchTerms.push(names[Math.floor(Math.random() * names.length)]);
   }
   const response = http.get(
-    `${__ENV.API_URL}/api/trpc/pitcher.byFuzzyName?batch=1&input={"0":{"json":"${searchTerms.join("%20")}"}}`,
+    `${
+      __ENV.API_URL
+    }/api/trpc/pitcher.byFuzzyName?batch=1&input={"0":{"json":"${searchTerms.join(
+      "%20"
+    )}"}}`,
     params
   );
 
   check(response, {
-    'Get response status is 200': (r) => r.status === 200,
+    "Get response status is 200": (r) => r.status === 200,
   });
-
-
 }
-
