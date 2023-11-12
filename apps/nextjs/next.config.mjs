5821c913098ebc72cf3c2d5bc5c0d65fb09b29ac
Tom Lamb <tmlamb@gmail.com>
2023-11-12 15:26:49 -0600

    next 14 changes

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,25 +1,13 @@
 import * as dotenv from 'dotenv'
 dotenv.config({ path: "../../.env" });
 
-// @ts-check
-import withTM from "next-transpile-modules";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@probable/api", "@probable/db", "@probable/common"],
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone",
+};
 
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-  return config;
-}
-
-export default withTM(["@probable/api", "@probable/db", "@probable/common"])(
-  defineNextConfig({
-    reactStrictMode: true,
-    swcMinify: true,
-    output: "standalone",
-  })
-);
+export default nextConfig;
+ 
